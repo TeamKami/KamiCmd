cbe895b7e5a111b53487c8a5fac28adad95fdc73
Zoin <zoinen@gmail.com>
2011-08-10 16:18:04 +0400

    Code cleanup in FilesDelegate

--- src/Panel_Classic/FilesDelegate.cpp
+++ src/Panel_Classic/FilesDelegate.cpp
@@ -3,13 +3,13 @@
 #include <QStyleOption>
 #include <QAbstractItemView>
 #include "FilesDelegate_win.h"
-
 #include "../IFileSystem.h"
 #include "FileListModel.h"
 #include <QTreeView>
 #include <QHeaderView>
 
 // This whole file is a pack of hacks and crutches
+
 #define VIEWPORT_MARGIN_LEFT 7/*13*/
 #define VIEWPORT_MARGIN_RIGHT 5/*13*/
 
@@ -17,13 +17,8 @@ void FilesDelegate::QCommonStylePrivate_viewItemDrawText(QStyle *style, QPainter
 {
 	const QWidget *widget = opt->widget;
 	const int textMargin = style->pixelMetric(QStyle::PM_FocusFrameHMargin, 0, widget) + 1;
-
-	// 1 in yp2 is a crutch and it works as it should somehow
-	QRect textRect = rect.adjusted(textMargin, 0, -textMargin, 1); // remove width padding
-
+	QRect textRect = rect.adjusted(textMargin, 0, -textMargin, 1); // remove width padding. Adjusted height by 1 is somehow what we need exactly
 	int textExtent = p->fontMetrics().width(opt->text);
-
- 	//p->drawRect(textRect);
 
 	// FIXME: colorize extension if found also
 
@@ -103,129 +98,120 @@ void FilesDelegate::paint( QPainter *p, const QStyleOptionViewItem &option, cons
 // 	style->drawControl(QStyle::CE_ItemViewItem, &opt, p, widget);
 // 	return;
 
-
-	if (const QStyleOptionViewItemV4 *vopt = &opt)
-	{
-		// Based on QWindowsVistaStyle::drawControl case CE_ItemViewItem
-
 #if defined(Q_OS_WIN32) || defined(Q_OS_WINCE)
-		QStyleOptionViewItemV4 adjustedOption = *vopt;
-		if (FilesDelegate_win_useVista(style))
-		{
-			QPalette palette = vopt->palette;
-			palette.setColor(QPalette::All, QPalette::HighlightedText, palette.color(QPalette::Active, QPalette::Text));
-			// Note that setting a saturated color here results in ugly XOR colors in the focus rect
-			palette.setColor(QPalette::All, QPalette::Highlight, palette.base().color().darker(108));
 
-			adjustedOption.palette = palette;
-			// We hide the focusrect in singleselection as it is not required
-			if ((view->selectionMode() == QAbstractItemView::SingleSelection) && !(vopt->state & QStyle::State_KeyboardFocusChange))
-				adjustedOption.state &= ~QStyle::State_HasFocus;
+	// Based on QWindowsVistaStyle::drawControl case CE_ItemViewItem
+	if (FilesDelegate_win_useVista(style))
+	{
+		QPalette palette = opt.palette;
+		palette.setColor(QPalette::All, QPalette::HighlightedText, palette.color(QPalette::Active, QPalette::Text));
+		// Note that setting a saturated color here results in ugly XOR colors in the focus rect
+		palette.setColor(QPalette::All, QPalette::Highlight, palette.base().color().darker(108));
 
-			opt = adjustedOption;
-			vopt = &opt;
-		}
+		opt.palette = palette;
+		// We hide the focusrect in singleselection as it is not required
+		if ((view->selectionMode() == QAbstractItemView::SingleSelection) && !(opt.state & QStyle::State_KeyboardFocusChange))
+			opt.state &= ~QStyle::State_HasFocus;
+	}
 #endif
 
-		// Custom padding for first and last columns; Also fixing viewItemPosition for swapped columns
-		if (!view->header()->logicalIndex(index.column()))
-		{
-			opt.rect.setLeft(opt.rect.left() + VIEWPORT_MARGIN_LEFT);
-			opt.viewItemPosition = QStyleOptionViewItemV4::Beginning;
-		}
-		if (view->header()->logicalIndex(index.column()) == index.model()->columnCount() - 1)
-		{
-			opt.rect.setRight(opt.rect.right() - VIEWPORT_MARGIN_RIGHT);
-			opt.viewItemPosition = QStyleOptionViewItemV4::End;
-		}
-
-
-		p->save();
-		p->setClipRect(opt.rect);
-
-		QRect checkRect = style->subElementRect(QStyle::SE_ItemViewItemCheckIndicator, vopt, widget);
-		QRect iconRect = style->subElementRect(QStyle::SE_ItemViewItemDecoration, vopt, widget);
-		QRect textRect = style->subElementRect(QStyle::SE_ItemViewItemText, vopt, widget);
-
-		if (vopt->state & QStyle::State_Selected)
-		{
-			opt.state &= ~(QStyle::State_Selected);
-			opt.backgroundBrush = QBrush(QColor(255, 215, 188)); // Soft orange-pink color for selection
-		}
-		if (vopt->state & QStyle::State_HasFocus || currentRow == index.row())
-			opt.state |= QStyle::State_Selected;
-
-		// draw the background
-		style->proxy()->drawPrimitive(QStyle::PE_PanelItemViewItem, &opt, p, widget);
-
-		// draw the check mark
-		if (vopt->features & QStyleOptionViewItemV2::HasCheckIndicator)
-		{
-			QStyleOptionViewItemV4 option(*vopt);
-			option.rect = checkRect;
-			option.state = option.state & ~QStyle::State_HasFocus;
-
-			switch (vopt->checkState)
-			{
-			case Qt::Unchecked:
-				option.state |= QStyle::State_Off;
-				break;
-			case Qt::PartiallyChecked:
-				option.state |= QStyle::State_NoChange;
-				break;
-			case Qt::Checked:
-				option.state |= QStyle::State_On;
-				break;
-			}
-			style->proxy()->drawPrimitive(QStyle::PE_IndicatorViewItemCheck, &option, p, widget);
-		}
-
-		// draw the icon
-		QIcon::Mode mode = QIcon::Normal;
-		if (!(vopt->state & QStyle::State_Enabled))
-			mode = QIcon::Disabled;
-		else if (vopt->state & QStyle::State_Selected)
-			mode = QIcon::Selected;
-		QIcon::State state = vopt->state & QStyle::State_Open ? QIcon::On : QIcon::Off;
-		vopt->icon.paint(p, iconRect, vopt->decorationAlignment, mode, state);
-
-		// draw the text
-		if (!vopt->text.isEmpty())
-		{
-
-			QPalette::ColorGroup cg = vopt->state & QStyle::State_Enabled ? QPalette::Normal : QPalette::Disabled;
-			if (cg == QPalette::Normal && !(vopt->state & QStyle::State_Active))
-				cg = QPalette::Inactive;
-
-			if (vopt->state & QStyle::State_Selected)
-				p->setPen(vopt->palette.color(cg, QPalette::HighlightedText));
-			else
-				p->setPen(vopt->palette.color(cg, QPalette::Text));
-
-			if (vopt->state & QStyle::State_Editing)
-			{
-				p->setPen(vopt->palette.color(cg, QPalette::Text));
-				p->drawRect(textRect.adjusted(0, 0, -1, -1));
-			}
-
-			QCommonStylePrivate_viewItemDrawText(style, p, vopt, textRect, !(index.data(FileListModel::AttributesRole).toUInt() & FileInfo::Directory));
-		}
-
-		// draw the focus rect
-// 		if (vopt->state & QStyle::State_HasFocus)
-// 		{
-// 			QStyleOptionFocusRect o;
-// 			o.QStyleOption::operator=(*vopt);
-// 			o.rect = style->subElementRect(QStyle::SE_ItemViewItemFocusRect, vopt, widget);
-// 			o.state |= QStyle::State_KeyboardFocusChange;
-// 			o.state |= QStyle::State_Item;
-// 			QPalette::ColorGroup cg = (vopt->state & QStyle::State_Enabled)	? QPalette::Normal : QPalette::Disabled;
-// 			o.backgroundColor = vopt->palette.color(cg, (vopt->state & QStyle::State_Selected) ? QPalette::Highlight : QPalette::Window);
-// 			style->proxy()->drawPrimitive(QStyle::PE_FrameFocusRect, &o, p, widget);
-// 		}
-
-		p->restore();
+	// Custom padding for first and last columns; Also fixing viewItemPosition for swapped columns
+	if (!view->header()->logicalIndex(index.column()))
+	{
+		opt.rect.setLeft(opt.rect.left() + VIEWPORT_MARGIN_LEFT);
+		opt.viewItemPosition = QStyleOptionViewItemV4::Beginning;
 	}
+	if (view->header()->logicalIndex(index.column()) == index.model()->columnCount() - 1)
+	{
+		opt.rect.setRight(opt.rect.right() - VIEWPORT_MARGIN_RIGHT);
+		opt.viewItemPosition = QStyleOptionViewItemV4::End;
+	}
+
+
+	p->save();
+	p->setClipRect(opt.rect);
+
+	QRect checkRect = style->subElementRect(QStyle::SE_ItemViewItemCheckIndicator, &opt, widget);
+	QRect iconRect = style->subElementRect(QStyle::SE_ItemViewItemDecoration, &opt, widget);
+	QRect textRect = style->subElementRect(QStyle::SE_ItemViewItemText, &opt, widget);
+
+	if (opt.state & QStyle::State_Selected)
+	{
+		opt.state &= ~(QStyle::State_Selected);
+		opt.backgroundBrush = QBrush(QColor(255, 215, 188)); // Soft orange-pink color for selection
+	}
+	if (opt.state & QStyle::State_HasFocus || currentRow == index.row())
+		opt.state |= QStyle::State_Selected;
+
+	// draw the background
+	style->proxy()->drawPrimitive(QStyle::PE_PanelItemViewItem, &opt, p, widget);
+
+	// draw the check mark
+	if (opt.features & QStyleOptionViewItemV2::HasCheckIndicator)
+	{
+		QStyleOptionViewItemV4 option(opt);
+		option.rect = checkRect;
+		option.state = option.state & ~QStyle::State_HasFocus;
+
+		switch (opt.checkState)
+		{
+		case Qt::Unchecked:
+			option.state |= QStyle::State_Off;
+			break;
+		case Qt::PartiallyChecked:
+			option.state |= QStyle::State_NoChange;
+			break;
+		case Qt::Checked:
+			option.state |= QStyle::State_On;
+			break;
+		}
+		style->proxy()->drawPrimitive(QStyle::PE_IndicatorViewItemCheck, &option, p, widget);
+	}
+
+	// draw the icon
+	QIcon::Mode mode = QIcon::Normal;
+	if (!(opt.state & QStyle::State_Enabled))
+		mode = QIcon::Disabled;
+	else if (opt.state & QStyle::State_Selected)
+		mode = QIcon::Selected;
+	QIcon::State state = opt.state & QStyle::State_Open ? QIcon::On : QIcon::Off;
+	opt.icon.paint(p, iconRect, opt.decorationAlignment, mode, state);
+
+	// draw the text
+	if (!opt.text.isEmpty())
+	{
+		QPalette::ColorGroup cg = opt.state & QStyle::State_Enabled ? QPalette::Normal : QPalette::Disabled;
+		if (cg == QPalette::Normal && !(opt.state & QStyle::State_Active))
+			cg = QPalette::Inactive;
+
+		if (opt.state & QStyle::State_Selected)
+			p->setPen(opt.palette.color(cg, QPalette::HighlightedText));
+		else
+			p->setPen(opt.palette.color(cg, QPalette::Text));
+
+		if (opt.state & QStyle::State_Editing)
+		{
+			p->setPen(opt.palette.color(cg, QPalette::Text));
+			p->drawRect(textRect.adjusted(0, 0, -1, -1));
+		}
+
+		QCommonStylePrivate_viewItemDrawText(style, p, &opt, textRect, !(index.data(FileListModel::AttributesRole).toUInt() & FileInfo::Directory));
+	}
+
+	// draw the focus rect
+// 	if (opt.state & QStyle::State_HasFocus)
+// 	{
+// 		QStyleOptionFocusRect o;
+// 		o.QStyleOption::operator=(opt);
+// 		o.rect = style->subElementRect(QStyle::SE_ItemViewItemFocusRect, &opt, widget);
+// 		o.state |= QStyle::State_KeyboardFocusChange;
+// 		o.state |= QStyle::State_Item;
+// 		QPalette::ColorGroup cg = (opt.state & QStyle::State_Enabled)	? QPalette::Normal : QPalette::Disabled;
+// 		o.backgroundColor = opt.palette.color(cg, (opt.state & QStyle::State_Selected) ? QPalette::Highlight : QPalette::Window);
+// 		style->proxy()->drawPrimitive(QStyle::PE_FrameFocusRect, &o, p, widget);
+// 	}
+
+	p->restore();
 }
 
 void FilesDelegate::PaletteChanged()
